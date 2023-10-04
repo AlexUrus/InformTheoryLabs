0c27c7f1c4dbfd6c426883747de1d85799252314
urus.urusik1337 <urus.urusik1337@gmail.com>
2023-10-04 12:52:19 +0700

    Remake hammingCode viewmodel

--- ViewModels/HammingCodeViewModel.cs
+++ ViewModels/HammingCodeViewModel.cs
@@ -26,14 +26,14 @@ namespace LR_1.ViewModels
             { 1, 1, 0, 1, 0, 0, 1}
         };
 
-
+        /*
         int[,] G = new int[4, 7]
         {
             { 1, 0, 0, 0, 1, 1, 1},
             { 0, 1, 0, 0, 1, 1, 0},
             { 0, 0, 1, 0, 0, 1, 1},
             { 0, 0, 0, 1, 1, 0, 1}
-        };
+        };*/
 
         private string _messageText;
         public string MessageText
@@ -68,7 +68,7 @@ namespace LR_1.ViewModels
         {
             if(MessageText != null && MessageText != string.Empty) 
             {
-                EncodeText = ApplyExtendedHammingCode(MessageText);
+
             }
         }
 
@@ -83,81 +83,109 @@ namespace LR_1.ViewModels
             return binaryStringBuilder.ToString();
         }
 
-        private string ApplyExtendedHammingCode(string input)
+        int[] G = { 8, 4, 2, 1, 11, 13, 7 };
+
+        public string BinaryCode(string message, string alphabet, string[] code)
         {
-            StringBuilder encodedString = new StringBuilder();
+            string binaryMessage = "";
 
-            // Пройдемся по каждому символу входной строки
-            foreach (char c in input)
+            for (int i = 0; i < message.Length; i++)
             {
-                // Конвертируем символ в его двоичное представление и добавляем нули слева до 16 бит
-                string binaryChar = Convert.ToString(c, 2).PadLeft(16, '0');
-
-                // Пройдемся по каждым 4 битам и применим расширенный код Хемминга
-                for (int i = 0; i < 16; i += 4)
+                int j = 0;
+                while (message[i] != alphabet[j])
                 {
-                    StringBuilder encodedGroup = new StringBuilder();
-                    for (int j = 0; j < 3; j++)
-                    {
-                        int sum = 0;
-                        for (int k = 0; k < 7; k++)
-                        {
-                            int bit = (binaryChar[i + k] - '0') & G[j, k];
-                            sum += bit;
-                        }
-                        encodedGroup.Append((sum % 2).ToString());
-                    }
-                    encodedString.Append(encodedGroup.ToString());
+                    j++;
                 }
+                binaryMessage += code[j];
             }
+            binaryMessage = Padding(binaryMessage);
 
-            return encodedString.ToString();
+            return binaryMessage;
         }
 
-        private string DecodeHammingCode(string input, int[,] hammingMatrix)
+        public string Padding(string binaryMessage)
         {
-            StringBuilder decodedString = new StringBuilder();
-            for (int i = 0; i < input.Length; i += 8)
+            binaryMessage += "1";
+            while (binaryMessage.Length % 4 != 0)
             {
-                int[] syndrome = new int[3];
-                int[] receivedData = new int[7];
-
-                // Заполняем массив приемных данных и вычисляем синдромы.
-                for (int j = 0; j < 7; j++)
-                {
-                    receivedData[j] = input[i + j + 1] - '0';
-                    for (int k = 0; k < 3; k++)
-                    {
-                        syndrome[k] += receivedData[j] & hammingMatrix[k, j];
-                    }
-                }
-
-                // Проверяем синдромы.
-                bool hasError = false;
-                for (int j = 0; j < 3; j++)
-                {
-                    syndrome[j] %= 2;
-                    if (syndrome[j] != 0)
-                    {
-                        hasError = true;
-                        break;
-                    }
-                }
-
-                // Если есть ошибка, пытаемся исправить ее.
-                if (hasError)
-                {
-                    int errorBit = syndrome[0] * 1 + syndrome[1] * 2 + syndrome[2] * 4;
-                    receivedData[errorBit - 1] ^= 1; // Инвертируем бит с ошибкой.
-                }
-
-                // Добавляем биты данных к результату.
-                for (int j = 0; j < 7; j++)
-                {
-                    decodedString.Append(receivedData[j]);
-                }
+                binaryMessage += "0";
             }
-            return decodedString.ToString();
+
+            return binaryMessage;
+        }
+
+        public int[] InfBits(string binaryCode)  //инф биты в группы по 4
+        {
+            int[] infBits = new int[binaryCode.Length / 4];
+            int i = 0;
+            while (binaryCode.Length != 0)
+            {
+                infBits[i] = Convert.ToInt32(binaryCode.Substring(0, 4), 2);
+                binaryCode = binaryCode.Substring(4);
+                i++;
+            }
+
+            return infBits;
+        }
+
+        static int SumXor(int x, int gMatrix)
+        {
+            int m = 0b1000;
+            int result = 0;
+            for (int i = 3; i >= 0; i--)
+            {
+                result ^= (x & gMatrix & (m >> (3 - i))) >> i;
+            }
+            return result;
+        }
+
+        public int[] CheckBits(int[] informativeBits)              //проверочный бит
+        {
+            int[] checkBits = new int[informativeBits.Length];
+
+
+            for (int i = 0; i < checkBits.Length; i++)
+            {
+                checkBits[i] = (SumXor(informativeBits[i], G[4]) << 2) + (SumXor(informativeBits[i], G[5]) << 1) + SumXor(informativeBits[i], G[6]);
+            }
+
+            return checkBits;
+        }
+
+        public int[] ParityBit(int[] infBits, int[] checkBits)   // бит четности
+        {
+            int[] parityBit = new int[checkBits.Length];
+
+            for (int i = 0; i < parityBit.Length; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    parityBit[i] ^= (infBits[i] & (1 << j)) >> j;
+
+                for (int j = 0; j < 3; j++)
+                    parityBit[i] ^= (checkBits[i] & (1 << j)) >> j;
+            }
+
+            return parityBit;
+        }
+
+        public int[] infBits { get; private set; }
+        public int[] checkBits { get; private set; }
+        public int[] parityBit { get; private set; }
+
+        public int[] getFullCode(string message, string alphabet, string[] code)
+        {
+            infBits = InfBits(BinaryCode(message, alphabet, code));
+            checkBits = CheckBits(infBits);
+            parityBit = ParityBit(infBits, checkBits);
+
+            int[] fullCode = new int[infBits.Length];
+
+            for (int i = 0; i < fullCode.Length; i++)
+            {
+                fullCode[i] += (infBits[i] << 4) + (checkBits[i] << 1) + (parityBit[i]);
+            }
+
+            return fullCode;
         }
     }
 }
