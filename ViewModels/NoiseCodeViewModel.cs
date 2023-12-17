57d843deacf0521bb2d5b8c40bc81c7ea0d490d8
urus.urusik1337 <urus.urusik1337@gmail.com>
2023-12-17 15:22:37 +0700

    add NoiseCode

---
+++ ViewModels/NoiseCodeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using LR_1.Tools;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_1.ViewModels
+{
+    public class NoiseCodeViewModel: BaseViewModel
+    {
+        private EntropyCalculation _model;
+        public ReactiveCommand<Unit, Unit> CalcEntropyCommand { get; }
+        public NoiseCodeViewModel()
+        {
+            CalcEntropyCommand = ReactiveCommand.Create(CalcEntropy);
+            _model = new EntropyCalculation();
+        }
+
+        public void CalcEntropy()
+        {
+            _model.CalcAllFields();
+        }
+    }
+}
